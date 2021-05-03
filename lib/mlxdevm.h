7016b372f1da6878fb5f025ab83430dde066d5b2
Parav Pandit <parav@nvidia.com>
2021-05-03 13:00:30 +0300

    mlxdevm: Initial commit of mlxdevm library
    Initial commit of mlxdevm library that supports SF port management.
    This includes
    (a) SF port creation,
    (b) SF function's mac address configure,
    (c) SF state get/set
    
    This is done over mlxdevm netlink interface similar to devlink
    interface. All related upstream devlink specific commands and
    attributes are applicable over mlxdevm netlink interface for SF.
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>

---
+++ lib/mlxdevm.h
@@ -0,0 +1,87 @@
+#ifndef _MLXDEVM_H_
+#define _MLXDEVM_H_
+
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <libmnl/libmnl.h>
+#include <linux/genetlink.h>
+
+#include "libnetlink.h"
+#include "mnl_utils.h"
+#include "utils.h"
+
+struct mlxdevm {
+	struct mnlu_gen_socket nlg;
+	char *bus;
+	char *dev;
+};
+
+/**
+ * mlxdevm_connect - Connect to the mlxdevm socket of kernel.
+ * 
+ * @dl_bus: mlxdevm instance bus name such as pci
+ * @dl_dev: mlxdevm instance device name such as 0000:03:00.0
+ *
+ * Connect to mlxdevm socket in kernel communication. On success
+ * it returns valid handle or returns NULL on error.
+ */
+struct mlxdevm *mlxdevm_open(const char *dl_sock_name,
+			     const char *dl_bus, const char *dl_dev);
+
+/**
+ * mlxdevm_close - Close a previously open mlxdevm connection.
+ * 
+ * Close a previously opened mlxdevm socket and frees the associated
+ * memory.
+ */
+void mlxdevm_close(struct mlxdevm *dl);
+
+/**
+ * mlxdevm_port - mlxdevm port to be allocated by port allocation API
+ * @port_index: port index assignend by the kernel for this port.
+ */
+struct mlxdevm_port {
+	uint32_t ndev_ifindex;
+	uint32_t port_index;
+	uint32_t pfnum;
+	uint32_t sfnum;
+	uint8_t mac_addr[6];
+	uint8_t state;
+	uint8_t opstate;
+};
+
+/**
+ * mlxdevm_sf_port_add - Add mlxdevm SF port
+ *
+ * Add mlxdevm SF port for specified PCI PF and SF number.
+ * It returns valid mlxdevm port on success or NULL on error.
+ */
+struct mlxdevm_port *
+mlxdevm_sf_port_add(struct mlxdevm *dl, uint32_t pfnum, uint32_t sfnum);
+
+/**
+ * mlxdevm_sf_port_del - Deleted previously created SF port
+ */
+void mlxdevm_sf_port_del(struct mlxdevm *dl, struct mlxdevm_port *port);
+
+/**
+ * mlxdevm_port_fn_mac_addr_set - Set the mlxdevm port's mac address
+ */
+int mlxdevm_port_fn_macaddr_set(struct mlxdevm *dl, struct mlxdevm_port *port,
+				const uint8_t *addr);
+
+int mlxdevm_port_fn_state_set(struct mlxdevm *dl, struct mlxdevm_port *port,
+			      uint8_t state);
+
+int mlxdevm_port_fn_state_get(struct mlxdevm *dl, struct mlxdevm_port *port,
+			      uint8_t *state, uint8_t *opstate);
+
+/**
+ * mlxdevm_port_fn_opstate_wait_attached - Wait for port function operational
+ * state to become active. Caller must first active the port function.
+ */
+int mlxdevm_port_fn_opstate_wait_attached(struct mlxdevm *dl,
+					  struct mlxdevm_port *port);
+
+#endif
